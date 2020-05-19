412169343a57e18a6ceec2465ac81e8f601b0ed4
goblintb <goblintb@mail.ru>
2020-05-19 13:10:42 +0300

    init commit

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class Test extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
